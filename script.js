2808529344ed7eab6fb78c4000e85c0e904e82a6
rpraja2 <studiomy182@gmail.com>
2024-01-05 18:37:01 -0800

    fix attractions and contact page

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 
     function submitForm() {
-        // Basic email verification
-        var email = document.getElementById('email').value;
-        var confirmEmail = document.getElementById('confirm-email').value;
+        
+        let email = document.getElementById('email').value;
+        let confirmEmail = document.getElementById('confirm-email').value;
 
         if (email !== confirmEmail) {
             alert('Email addresses do not match. Please check and try again.');
             return;
         }
-
-        // If email addresses match, you can proceed with form submission or other actions
-        // In a real-world scenario, this is where you would send the form data to the server
-
         alert('Form submitted successfully!');
     }
